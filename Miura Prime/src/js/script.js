22642b9e8d39238fac2e71f7e705c94ad28a1be3
Gabriel Miura <81529174+cbmiura@users.noreply.github.com>
2021-09-15 05:42:43 -0300

    Delete Miura Prime directory

--- Miura Prime/src/js/script.js
+++
@@ -1,8 +0,0 @@
-function click() {
-   var menu = document.getElementById('contentMenu');
-   if (menu.className == "contentMenuOff") {
-       menu.className = "contentMenuOn";
-   } else {
-       menu.className = "contentMenuOff";
-   }
-}
